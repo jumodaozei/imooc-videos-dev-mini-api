116529da0a76d2191187486bd833e41d91434b48
75c_100006496246_bastenyang <75c_100006496246_bastenyang@git.cloud.tencent.com>
2018-09-10 18:43:22 +0800

    实现用户的登录和注销

---
+++ src/main/java/com/imooc/controller/BasicController.java
@@ -0,0 +1,16 @@
+package com.imooc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.imooc.utils.RedisOperator;
+
+@RestController
+public class BasicController {
+	
+	@Autowired
+	public RedisOperator redis;
+	
+	public static final String USER_REDIS_SESSION = "user-redis-session";
+	
+}
--- src/main/java/com/imooc/controller/RegistLoginController.java
+++ src/main/java/com/imooc/controller/RegistLoginController.java
@@ -1,27 +1,32 @@
 package com.imooc.controller;
 
+import java.util.UUID;
+
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.imooc.pojo.Users;
+import com.imooc.pojo.vo.UsersVO;
 import com.imooc.service.UserService;
 import com.imooc.utils.IMoocJSONResult;
 import com.imooc.utils.MD5Utils;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
 @Api(value = "用户注册登录接口", tags = { "注册和登录的controller" })
-public class RegistLoginController {
+public class RegistLoginController extends BasicController {
 
 	@Autowired
 	private UserService userServicel;
 
-	@ApiOperation(value="用户注册",notes="用户注册的接口")
+	@ApiOperation(value = "用户注册", notes = "用户注册的接口")
 	@PostMapping("/regist")
 	public IMoocJSONResult regist(@RequestBody Users user) throws Exception {
 		// 1 判断用户名密码必须不为空
@@ -42,29 +47,52 @@ public class RegistLoginController {
 		} else {
 			return IMoocJSONResult.errorMsg("用户名已存在！");
 		}
-		user.setPassword("");//安全性问题 不返回用户密码
-		return IMoocJSONResult.ok(user);
+		user.setPassword("");// 安全性问题 不返回用户密码
+
+		UsersVO userVO = setUserRedisSessionToken(user);
+
+		return IMoocJSONResult.ok(userVO);
 	}
-	
-	@ApiOperation(value="用户登录",notes="用户登录的接口")
+
+	// 注册时保存用户session
+	public UsersVO setUserRedisSessionToken(Users userModel) {
+		String uniqueToken = UUID.randomUUID().toString();
+		redis.set(USER_REDIS_SESSION + ":" + userModel.getId(), uniqueToken, 1000 * 60 * 30);
+
+		UsersVO userVO = new UsersVO();
+		BeanUtils.copyProperties(userModel, userVO);
+		userVO.setUserToken(uniqueToken);
+		return userVO;
+	}
+
+	@ApiOperation(value = "用户登录", notes = "用户登录的接口")
 	@PostMapping("/login")
 	public IMoocJSONResult login(@RequestBody Users user) throws Exception {
-		
-		//1 判空
-		if(StringUtils.isBlank(user.getUsername()) || StringUtils.isBlank(user.getPassword())) {
+
+		// 1 判空
+		if (StringUtils.isBlank(user.getUsername()) || StringUtils.isBlank(user.getPassword())) {
 			return IMoocJSONResult.errorMsg("用户名或密码不能为空！");
 		}
-		
-		//2 判断用户是否存在
+
+		// 2 判断用户是否存在
 		Users userResult = userServicel.queryUserForLogin(user.getUsername(), MD5Utils.getMD5Str(user.getPassword()));
-		
-		//3 返回
-		if(userResult != null) {
+
+		// 3 返回
+		if (userResult != null) {
 			userResult.setPassword("");
-			return IMoocJSONResult.ok(userResult);
-		}else {
+			UsersVO userVO = setUserRedisSessionToken(userResult);
+			return IMoocJSONResult.ok(userVO);
+		} else {
 			return IMoocJSONResult.errorMsg("用户不存在或用户名密码不对！请重新输入！");
 		}
+	}
+
+	@ApiOperation(value = "用户注销", notes = "用户注销的接口")
+	@ApiImplicitParam(name = "userId", value = "用户id", required = true, dataType = "String", paramType = "query")
+	@PostMapping("/logout")
+	public IMoocJSONResult logout(String userId) throws Exception {
+		redis.del(USER_REDIS_SESSION + ":" + userId);
+		return IMoocJSONResult.ok();
 	}
 
 }
