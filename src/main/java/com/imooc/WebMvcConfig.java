20633d200472e3731931534dc5f3bf28b82a60f0
75c_100006496246_bastenyang <75c_100006496246_bastenyang@git.cloud.tencent.com>
2018-09-20 17:25:03 +0800

    注册拦截器

--- src/main/java/com/imooc/WebMvcConfig.java
+++ src/main/java/com/imooc/WebMvcConfig.java
@@ -1,8 +1,12 @@
 package com.imooc;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.imooc.controller.interceptor.MiniInterceptor;
 
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
@@ -12,6 +16,17 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 		registry.addResourceHandler("/**")
 				.addResourceLocations("classpath:/META-INF/resources/")
 				.addResourceLocations("file:D:/imooc-videos-dev/");
+	}
+	
+	@Bean
+	public MiniInterceptor miniInterceptor() {
+		return new MiniInterceptor();
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(miniInterceptor()).addPathPatterns("/user/**");
+		super.addInterceptors(registry);
 	}
 
 }
