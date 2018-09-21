2c617a3be299de27c4f33492bdfca4ba5b998e33
75c_100006496246_bastenyang <75c_100006496246_bastenyang@git.cloud.tencent.com>
2018-09-21 10:54:29 +0800

    拦截userlike userunlike

--- src/main/java/com/imooc/WebMvcConfig.java
+++ src/main/java/com/imooc/WebMvcConfig.java
@@ -27,6 +27,7 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(miniInterceptor()).addPathPatterns("/user/**")
 						.addPathPatterns("/video/upload","/video/uploadCover")
+						.addPathPatterns("/video/userLike","/video/userUnLike")
 												   .addPathPatterns("/bgm/**");
 		super.addInterceptors(registry);
 	}
